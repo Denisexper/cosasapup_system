4a9a6c7410100b469e6a73b256d01f13f7c4dda5
Denisexper <clasic.exper2023@gmail.com>
2025-05-12 21:24:05 -0600

    subiendo

---
+++ system_cosasapup/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace system_cosasapup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
